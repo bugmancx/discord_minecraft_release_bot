479f9e6a586fa6789b1e7caa5b11c6d54fc5361e
bugmancx <bugmancx@gmail.com>
2020-12-02 10:28:22 +0000

    Significant overhaul, better configuration, better checks

--- README.md
+++ README.md
@@ -4,17 +4,56 @@ A Discord Bot which checks for whether a new Minecraft release or snapshot has b
 The script will extract the "snapshot" and "release" version information from the Mojang version manifest file used by the Minecraft launcher and compare it against a cached copy. If it detects a variation, a message will be posted to Discord.
 
 
+# Configuration File
+
+The source code includes an example configuration file specifying the three required configurable options.
+
+ID: The unique identifier for this bot instance. This is useful if running multiple instances of the bot for multiple discord servers.
+WEBHOOK_URL: The URL of the webhook used for posting to the Discord server.
+
+Optional:
+
+TMP: Path to your temporary files location.
+MESSAGE_CONTENT: This is the text string posted to the Discord server. If you want anything more than the basic message you need to ensure that it's in the appropriate format to be included in the JSON payload. For example including \n for newlines.
+
+e.g.
+
+```
+MESSAGE_CONTENT="A new Minecraft $VERB is available! | Release: $MANIFEST_VERSION_RELEASE | Snapshot: $MANIFEST_VERSION_SNAPSHOT\n\nEnjoy yourselves!"
+```
+
+RELEASE_VERB: Override the formatting/wording of the word "release" to your liking.
+SNAPSHOT_VERB: Override the formatting/wording of the word "snapshot" to your liking.
+
+e.g.
+
+```
+RELEASE_VERB=__release__
+```
+
+DISCORD_USERNAME: The username that the Bot will use in Discord when posting.
+
 # Installation Instructions
-1. Set up a Discord Webhook in your channel. See: https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
-2. Add your webhook URL to the DISCORD_WEBHOOK_URL variable within the script. 
-_Bash version: Ensure the URL is wrapped around single quotes inside the variable, or the curl command will not work. (An example is provided within the script.)_
-3. Add the script to cron to run at your convenience, but please use discretion and don't be excessive.
+2. Create a configuration file and include the three variables for ID, DISCORD_USERNAME and WEBHOOK_URL as per the example.
+2. Set up a Discord Webhook in your channel. See: https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+3. Add your webhook URL to the WEBHOOK_URL variable within the configuration file.
+4. Add the script to cron to run at your convenience, but please use discretion as each run will fetch the version manifest.
+
+Cron Example:
+
+```
+*/15 * * * *  ~/bots/discord_minecraft_release_bot.sh -c ~/bots/myinstance.conf
+```
 
 
 # Notes
 - The default bot username is "Minecraft" but this can be changed by modifying the DISCORD_USERNAME variable.
-- The output message is also configurable if you want, by changing the contents of DISCORD_MESSAGE towards the end of the script.
-- You can modify the temporary location where the script should store its cache file in the TMP variable.
+- You can override the temporary location where the script should store its cache file by adding a TMP variable to the configuration file.
+- The output message is also configurable if you want, just add this to your configuration file with the correct syntax:
+
+```
+MESSAGE_CONTENT="A new Minecraft $VERB is available! | Release: $MANIFEST_VERSION_RELEASE | Snapshot: $MANIFEST_VERSION_SNAPSHOT\n\nEnjoy yourselves!"
+```
 
 # Requirements
 Bash Version:
@@ -22,3 +61,7 @@ Bash Version:
 curl: https://curl.haxx.se/
 
 jq: https://stedolan.github.io/jq/
+
+# Contributors
+
+Python variation contributed by TheUnlocked based on first version of the script. May be out of date.
--- discord_minecraft_release_bot.sh
+++ discord_minecraft_release_bot.sh
@@ -1,61 +1,172 @@
 #!/bin/bash
 # Script written by bugmancx
 
-#User Configurable Variables
+#Default Configurable Variables
 TMP=/tmp
 MANIFEST_VERSION_URL='https://launchermeta.mojang.com/mc/game/version_manifest.json'
-DISCORD_USERNAME="Minecraft"
-#DISCORD_WEBHOOK_URL='https://discordapp.com/api/webhooks/example/webhook'
-## Note - Discord Webhook URL must be contained within single quotes within the variable in order to work with curl.
-DISCORD_WEBHOOK_URL=''
 
-#Setup Variables
-CACHED_FILE="$TMP/cached_version_manifest.json"
-MANIFEST_FILE="$TMP/version_manifest.json"
-DIFF=0
 
-# Retrieve current release values from cached copy on filesystem
-if [ -f $CACHED_FILE ] ; then
-  CACHED_VERSION_RELEASE=$(jq -r '.latest."release"' $CACHED_FILE)
-  CACHED_VERSION_SNAPSHOT=$(jq -r '.latest."snapshot"' $CACHED_FILE)
-else
-  CACHED_VERSION_RELEASE="unknown"
-  CACHED_VERSION_SNAPSHOT="unknown"
+# Localised configuration
+usage() {
+echo "Usage: cmd [-c] <config file>"
+}
+
+while getopts "c:v" opt; do
+  case ${opt} in
+    c ) # Config file
+        CONFIG_FILE=${OPTARG}
+        #echo "Working with $CONFIG_FILE"
+      if [ ! -f $CONFIG_FILE ] ; then
+        echo "Configuration file $CONFIG_FILE not found!"
+        exit
+      else
+        source $CONFIG_FILE
+      fi
+      ;;
+    v ) # Verbose mode
+      VERBOSE=1 # For future debug expansion
+      ;;
+    \? ) 
+      usage
+      ;;
+  esac
+done
+
+if ((OPTIND == 1))
+then
+    echo "No options specified"
+    usage
+    exit
 fi
 
-# Retrieve latest version from Mojang manifest
+
+
+validate_configuration() {
+  # Check that variables are OK
+  if [ -z ${ID+x} ]; then echo "ID is not set."; SYNTAX=1 ;
+  else 
+    SED_ID=$(echo $ID | sed 's/[^[:alnum:]]\+//g') # Ensure string is only letters and or numbers
+    ID=$SED_ID
+  fi
+
+  if [ -z ${WEBHOOK_URL+x} ]; then echo "WEBHOOK_URL is not set."; SYNTAX=1 ; fi
+  
+  if [ -n $SYNTAX ] ; then
+    SYNTAX=0 # No issues found
+  fi
+
+  if [ $SYNTAX -eq 1 ] ; then
+    echo ; echo "Configuration file is not valid. Please check and run again."
+    exit
+  fi
+}
+
+## Start working
+##################################################################
+
+## CONSTRUCT VARIABLES
+CACHED_FILE="$TMP/$ID.cached_version_manifest.json"
+MANIFEST_FILE="$TMP/$ID.version_manifest.json"
+DIFF=0
+FIRST_RUN=0
+
+
+## SET FUNCTIONS
+
+function get_manifest() {
 curl -s -o $MANIFEST_FILE $MANIFEST_VERSION_URL
 ## Write this to disk; we'll use it twice and want to reduce calls against the Mojang API.
+}
 
+
+function retrieve_and_set_values() {
 MANIFEST_VERSION_RELEASE=$(jq -r '.latest."release"' $MANIFEST_FILE)
 MANIFEST_VERSION_SNAPSHOT=$(jq -r '.latest."snapshot"' $MANIFEST_FILE)
 
+CACHED_VERSION_RELEASE=$(jq -r '.latest."release"' $CACHED_FILE)
+CACHED_VERSION_SNAPSHOT=$(jq -r '.latest."snapshot"' $CACHED_FILE)
+}
 
-# Compare Release
 
+function manifest_compare() {
 if [ ! "$CACHED_VERSION_RELEASE" == "$MANIFEST_VERSION_RELEASE" ] ; then
-  # Differences detected in RELEASE
+  echo "Differences were detected in RELEASE"
   DIFF=1
+  if [ ! -z ${RELEASE_VERB+x} ] ; then
+    VERB=$RELEASE_VERB
+  else
+    VERB="**release**"
+  fi
 fi
 
 if [ ! "$CACHED_VERSION_SNAPSHOT" == "$MANIFEST_VERSION_SNAPSHOT" ] ; then
-  # Differences detected in SNAPSHOT
+  echo "Differences were detected in SNAPSHOT"
   DIFF=1
+  if [ ! -z ${SNAPSHOT_VERB+x} ] ; then
+    VERB=$SNAPSHOT_VERB
+  else
+    VERB="**snapshot**"
+  fi
 fi
+}
+
+if [ -z ${DISCORD_USERNAME+x} ]; 
+  then DISCORD_USERNAME="Minecraft" # Default
+fi
+
+function discord_post () {
+curl -H "Content-Type: application/json" \
+ -X POST \
+ -d "{\"username\": \"$DISCORD_USERNAME\", \"content\": \"$MESSAGE_CONTENT\"}" {$WEBHOOK_URL}
+}
+
+function update_cache() {
+  mv $MANIFEST_FILE $CACHED_FILE
+}
+
+##############################################################################
+
+
+## NOW GO, REALLY.
+
+validate_configuration
+get_manifest
+retrieve_and_set_values
+
+# Retrieve latest version from Mojang manifest
+if [ ! -f $CACHED_FILE ] ; then
+  echo "WARNING: No cached manifest exists. This may be our first run."
+  echo "No results will be posted until the next manifest update from Mojang."
+  FIRST_RUN=1
+  update_cache
+  exit
+fi
+
+# Compare Release
+VERB="version" # Default verb if for some reason type detection fails
+
+manifest_compare
+
 
 # If differences detected, do Discord
 if [ $DIFF -gt 0 ] ; then
+  if [ -z ${MESSAGE_CONTENT+x} ] ; then # If not already set in config file
+    echo "MC=$MESSAGE_CONTENT"
+    # Specify the default wording for the bot's custom message.
+    MESSAGE_CONTENT="A new Minecraft $VERB is available! | Release: $MANIFEST_VERSION_RELEASE | Snapshot: $MANIFEST_VERSION_SNAPSHOT"
+  else
+    source $CONFIG_FILE # Freshen the MESSAGE_CONTENT now that other vars are populated
+  fi
 
-  # Specify the wording for the bot's custom message.
-  MESSAGE_CONTENT="Release: $MANIFEST_VERSION_RELEASE | Snapshot: $MANIFEST_VERSION_SNAPSHOT"
+  if [ ! $FIRST_RUN -eq 1 ] ; then
+    # Looks like this is our first run, so just exit here and don't post anything.
+    # Post to Discord
+    discord_post
+  fi
 
-  # Post to Discord
-  curl -H "Content-Type: application/json" \
-  -X POST \
-  -d "{\"username\": \"$DISCORD_USERNAME\", \"content\": \"$MESSAGE_CONTENT\"}" $DISCORD_WEBHOOK_URL
-
-  # Make manifest new cached manifest.
-  mv $MANIFEST_FILE $CACHED_FILE
+# Make manifest new cached manifest.
+update_cache
 
 fi
 
+if [ -f $MANIFEST_FILE ] ; then rm $MANIFEST_FILE ; fi
